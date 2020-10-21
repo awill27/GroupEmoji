845c10a04d4d9222ebe171b1a2bfcc7b7c79e8ed
Ashley Williams <awilliams1026@student.ccc.edu>
2020-10-20 19:44:29 -0500

    Halfway Mark

---
+++ GroupEmoji/Emoji.swift
@@ -0,0 +1,22 @@
+//
+//  Emoji.swift
+//  GroupEmoji
+//
+//  Created by MXC Swift on 10/20/20.
+//
+
+import Foundation
+
+struct Emoji {
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+    
+    init(symbol: String, name: String, description: String, usage: String) {
+        self.symbol = symbol
+        self.name = name
+        self.description = description
+        self.usage = usage
+    }
+}
